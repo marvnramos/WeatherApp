79b2e5e6c848f5f0e602bb414b4fc3352f172a91
Marvin Ramos <mr487223@gmail.com>
2024-08-25 01:12:58 -0600

    feat(getWeatherImages): add fun which return a painter base on weather

--- app/src/main/java/com/example/weatherapp/utils/funs.kt
+++ app/src/main/java/com/example/weatherapp/utils/funs.kt
@@ -1,6 +1,10 @@
 package com.example.weatherapp.utils
 
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.example.weatherapp.R
 
 
 fun hexToColorInt(hex: String): Color {
@@ -24,4 +28,41 @@ fun hexToColorInt(hex: String): Color {
 fun getCelciusFromKelvin(kelvin: Double): Double {
     val celsius = kelvin - 273.15
     return String.format("%.2f", celsius).toDouble()
+}
+
+@Composable
+fun getWeatherImages(weatherDescription: String): Painter {
+    return when {
+        weatherDescription.contains("cloud", ignoreCase = true) ||
+                weatherDescription.contains(
+                    "nubes",
+                    ignoreCase = true
+                ) -> painterResource(id = R.drawable.day_clouds)
+
+        weatherDescription.contains("rain", ignoreCase = true) ||
+                weatherDescription.contains(
+                    "lluvia",
+                    ignoreCase = true
+                ) -> painterResource(id = R.drawable.day_rain)
+
+        weatherDescription.contains("storm", ignoreCase = true) ||
+                weatherDescription.contains(
+                    "tormenta",
+                    ignoreCase = true
+                ) -> painterResource(id = R.drawable.day_storm)
+
+        weatherDescription.contains("sun", ignoreCase = true) ||
+                weatherDescription.contains(
+                    "sol",
+                    ignoreCase = true
+                ) -> painterResource(id = R.drawable.day_sun)
+
+        weatherDescription.contains("wind", ignoreCase = true) ||
+                weatherDescription.contains(
+                    "viento",
+                    ignoreCase = true
+                ) -> painterResource(id = R.drawable.day_wind)
+
+        else -> painterResource(id = R.drawable.day_snow)
+    }
 }
